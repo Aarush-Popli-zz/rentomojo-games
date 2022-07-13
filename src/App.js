6fc375f3e2b5bf17b2cd7e5d5028acab9fd2c9b2
Aarush Popli <popliaarush00@gmail.com>
2022-07-13 16:53:58 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
+import About from './components/About';
+import Games from './components/Games';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/" element={<Games />}></Route>
+          </Routes>
+      </Router>
+    </>
   );
 }
 
